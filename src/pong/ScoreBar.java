32697c7bf8faaf31109ebb75b416d425682fc536
542331 <542331@DHS-K103-28.District.FBISD.Local>
2014-05-23 10:09:48 -0500

    Fixed javaDoc

--- src/pong/ScoreBar.java
+++ src/pong/ScoreBar.java
@@ -17,39 +17,46 @@ import javax.swing.JComponent;
 public class ScoreBar extends JComponent{
     int score1; //score of player 1
     int score2; // score of player 2
+    /**
+     *Creates a new ScoreBar with both players scores as 0
+     */
     public ScoreBar(){
         score1=0;
         score2=0;
     }
+    /**
+     * Draws the ScoreBar on the JFrame
+     * @param g 
+     */
     public void paintComponent(Graphics g) {
         g.drawRect(0, 0, this.getWidth()-1, this.getHeight()-1); // draws a border
         String s = "Player 1: " + score1 + "                  Player 2: " + score2; 
         g.drawString(s,25,getHeight()-4);
     }
-    /*
+    /**
      * Increments the score of player1 and redraws the scoreboard
      */
     public void increment1(){
         score1++;
         repaint();
     }
-    /*
+    /**
      * Increments the score of player1 and redraws the scoreboard
      */
     public void increment2(){
         score2++;
         repaint();
     }
-    /*
-     * Gets a JButton that will reset this. 
+    /**
+     * @return JButton that has a ResetListener for this object attached. 
      */
     public JButton getReset(){
         JButton reset = new JButton("Reset score");
         reset.addActionListener(new ResetListener());
         return reset;
     }
-    /*
-     * A listener that resets the score.
+    /**
+     * @return A listener that resets the score.
      */
     class ResetListener implements ActionListener{
         @Override
